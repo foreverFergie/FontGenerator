a2825e3d7fafb5c13fa137b40e6fe18976add719
Erinn <erinnf44@gmail.com>
2020-11-30 12:14:21 -0500

    misc

--- ImageProcessing/font_class.py
+++ ImageProcessing/font_class.py
@@ -8,6 +8,7 @@ class TTF:
         :param imgStore: Img_storage object full of filepaths of SVGs
         """
         self.imgSVGs =imgStore
+        self.baseFont=fontforge.font()  #empty font for adding in new glyphs
 
 
     def addSVGs(self):
---
+++ ImageProcessing/run_ttf.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('cmd /k "fontforge -lang=py -script font_class')
